a929bd6cad507e9115f7bb9fa600962cca7f10b4
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2024-02-19 22:41:43 +0900

    add line

--- gohantabetabe.py
+++ gohantabetabe.py
@@ -6,8 +6,18 @@ yo = ["パスタ", "オムライス", "ハンバーグ"]
 chu = ["麻婆豆腐", "油淋鶏", "餃子"]
 
 dinner = rdm.choice(genre)
+wachoice = rdm.choice(wa)
+yochoice = rdm.choice(yo)
+chuchoice = rdm.choice(chu)
+
 decide = input("今日のご飯は"+dinner+"でどう？>>y/n")
 if decide == "y":
     print("いっぱい", dinner, "食べるぞ！")
+    if dinner == "中華":
+        print(chuchoice, "にしよう！")
+    if dinner == "洋食":
+        print(yochoice, "にしよう！")
+    if dinner == "和食":
+        print(wachoice, "にしよう！")
 else:
-    print("何食べようかなあ")
+    print("じゃあ", dinner, "は？")
