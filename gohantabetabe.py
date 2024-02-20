ad7893c6730dc01adcda581cc931aa6c5d1d0cb3
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2024-02-20 23:17:30 +0900

    making roop

--- gohantabetabe.py
+++ gohantabetabe.py
@@ -5,22 +5,21 @@ wa = ["お寿司", "うどん", "そば"]
 yo = ["パスタ", "オムライス", "ハンバーグ"]
 chu = ["麻婆豆腐", "油淋鶏", "餃子"]
 
-dinner = rdm.choice(genre)
-wachoice = rdm.choice(wa)
-yochoice = rdm.choice(yo)
-chuchoice = rdm.choice(chu)
+while True:
+    dinner = rdm.choice(genre)
+    wachoice = rdm.choice(wa)
+    yochoice = rdm.choice(yo)
+    chuchoice = rdm.choice(chu)
 
-decide = input("今日のご飯は"+dinner+"でどう？>>y/n")
-if decide == "y":
-    print("いっぱい", dinner, "食べるぞ！")
-    if dinner == "中華":
-        print(chuchoice, "にしよう！")
-    if dinner == "洋食":
-        print(yochoice, "にしよう！")
-    if dinner == "和食":
-        print(wachoice, "にしよう！")
-else:
-    print("そんなこと言うなら自分で決めや〜")
-
-#0220
-#ループできるようにする
+    decide = input("今日のご飯は"+dinner+"でどう？>>y/n")
+    if decide == "y":
+        print("いっぱい", dinner, "食べるぞ！")
+        if dinner == "中華":
+            print(chuchoice, "にしよう！")
+            break
+        if dinner == "洋食":
+            print(yochoice, "にしよう！")
+            break
+        if dinner == "和食":
+            print(wachoice, "にしよう！")
+            break
