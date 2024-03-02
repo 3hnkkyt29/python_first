bf2ce71cf83f511d2682c8d5e692df038a9d6f34
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2024-03-02 15:32:21 +0900

    part1

---
+++ basic_test/ch3.py
@@ -0,0 +1,20 @@
+#part1
+n1 = 50 - (5*6)
+print(n1)
+print(type(n1)) #int
+
+n2 = 20/5
+print(n2)
+print(type(n2)) #float
+n3 = 20//5
+print(n3)
+print(type(n3)) #int
+
+print(9**2)
+
+n4 = (3 + 5j)*(3 - 5j)
+print(n4)
+
+n5 = 0.1
+n6 = 0.2
+print(n5 + n6) #10進数のあれこれでなんか上手く0.3にはならない
