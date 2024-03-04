bada7b07bb9b53258deb02cfe29a4d87f9a3fa4a
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2024-03-04 19:46:48 +0900

    finish part2

--- basic_test/ch4.py
+++ basic_test/ch4.py
@@ -11,3 +11,24 @@ print(users)
 #    if status == "inactive":
 #        del users[user]
 #print(users)
+#↑copy()がないとエラーになるよ
+
+#part2
+for i in range(1,10):
+    for j in range(1,10):
+        print(i*j, end=",")
+    else:
+        print("\n", str(i), "の段でした")
+
+for i in range(2,10):
+    if i % 2 ==0:
+        print(i, "は偶数")
+        continue
+    print(i, "は奇数")
+
+for i in range(2,10):
+    if i % 2 ==0:
+        print(i, "は偶数")
+        break
+    print(i, "は奇数")
+#continueだとループの最初に戻るけどbreakだともう完全にループから抜ける
