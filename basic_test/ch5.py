11017fed9a5a7b45fe81e3cce6c2c269a124900d
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2024-03-06 21:19:36 +0900

    finish ch5 part2

---
+++ basic_test/ch5.py
@@ -0,0 +1,23 @@
+#part2
+t = 10, "a", [1,2]
+print(t)
+
+v = 123, 456, "hello"
+a, b, c = v
+print(a)
+print(b)
+print(c)
+
+set1 = {1, 2, 3, "a"}
+set2 = {4, 5, 6, "a"}
+wa = set1 | set2
+print(wa)
+sa = set1 - set2
+print(sa)
+seki = set1 & set2
+print(seki)
+taisyo = set1 ^ set2
+print(taisyo)
+
+
+
