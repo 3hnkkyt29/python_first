57e6ef27e88ab566e735ca01a4bb58eccf2664f5
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2024-03-24 21:26:30 +0900

    commit

--- basic_test/prac.py
+++ basic_test/prac.py
@@ -1,6 +1,25 @@
-set = {'pine', 'apple', 'apple', 'pen'}
-print(set)
+# set = {'pine', 'apple', 'apple', 'pen'}
+# print(set)
 
-num = {'one': 1, 'two':2}
-num['three'] = 3
-print(num['three'])
+# num = {'one': 1, 'two':2}
+# num['three'] = 3
+# print(num['three'])
+
+# for i in range(4):
+#     if i == 2:
+#         continue
+#     print(i)
+
+# num = [1, 2, 3, 4]
+# num.pop(2)
+# print(num)
+
+# num = [1, 2, 3]
+# del num[:1]
+# print(num)
+
+a = 'Python'
+b = 'programing'
+print(f'{a} is a {b} language')
+print('{0} is a {1} language'.format(a, b))
+print('%s is a %s language' % (a, b))
